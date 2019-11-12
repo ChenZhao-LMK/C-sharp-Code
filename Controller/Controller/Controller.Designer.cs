23e43ac5944f00acf6aee2938602d5218ef79c83
ChenZhao <chen_zhao_heu@163.com>
2019-11-12 18:38:40 +0800

    自定义控件

---
+++ Controller/Controller/Controller.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Controller
+{
+    partial class Controller
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Controller));
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(340, 4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = " ";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(40, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 28);
+            this.button1.TabIndex = 3;
+            this.button1.Text = " ";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(170, 2);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = " ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Location = new System.Drawing.Point(468, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(30, 28);
+            this.button2.TabIndex = 4;
+            this.button2.Text = " ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Controller
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.MinimumSize = new System.Drawing.Size(200, 0);
+            this.Name = "Controller";
+            this.Size = new System.Drawing.Size(500, 30);
+            this.Load += new System.EventHandler(this.Controller_Load);
+            this.Resize += new System.EventHandler(this.Controller_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Controller/Controller/Controller.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Controller
+{
+    public partial class Controller : UserControl
+    {
+        public Controller()
+        {
+            InitializeComponent();
+        }
+
+        static string[] str = new string[] { "C#1", "C#2", "C#3", "C#4" };
+        ArrayList arrayList = new ArrayList();
+        static int Index = 0;
+        int c;
+
+        private void Controller_Load(object sender, EventArgs e)//控件初始加载
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                arrayList.Add(str[i]);
+            }
+            c = arrayList.Count;
+            this.label1.Text = str[0];
+            this.label2.Text = str[1];
+            this.label3.Text = str[2];
+
+            
+
+        }
+
+
+
+        //public void GetStr()
+        //{
+
+        //    for (int i = 0; i < str.Length; i++)
+        //    {
+        //        arrayList.Add(str[i]);
+        //    }
+        //    c = arrayList.Count;
+        //}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ++Index;
+            if (Index >= c) Index -= c;
+            label1.Text = (string)arrayList[Index < c ? Index : Index - c];
+            label2.Text = (string)arrayList[Index + 1 < c ? Index + 1 : Index + 1 - c];
+            label3.Text = (string)arrayList[Index + 2 < c ? Index + 2 : Index + 2 - c];
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            --Index;
+            if (Index <= 0) Index += c;
+            label1.Text = (string)arrayList[Index < c ? Index : Index - c];
+            label2.Text = (string)arrayList[Index + 1 < c ? Index + 1 : Index + 1 - c];
+            label3.Text = (string)arrayList[Index + 2 < c ? Index + 2 : Index + 2 - c];
+        }
+
+        private void Controller_Resize(object sender, EventArgs e)
+        {
+            button1.Location = new System.Drawing.Point(ClientRectangle.Left, ClientRectangle.Top);
+            button1.Size = new System.Drawing.Size(ClientRectangle.Height, ClientRectangle.Height);
+
+            label1.Location = new System.Drawing.Point(ClientRectangle.Left + 40, ClientRectangle.Top + 3);
+            label1.Size = new System.Drawing.Size((int)((ClientRectangle.Width - 100) * 0.3), ClientRectangle.Height - 6);
+
+            label2.Location = new System.Drawing.Point(label1.Left + label1.Width + 10, ClientRectangle.Top + 1);
+            label2.Size = new System.Drawing.Size((int)((ClientRectangle.Width - 100) * 0.4), ClientRectangle.Height - 2);
+
+            label3.Location = new System.Drawing.Point(label2.Left + label2.Width + 10, ClientRectangle.Top + 3);
+            label3.Size = new System.Drawing.Size((int)((ClientRectangle.Width - 100) * 0.3), ClientRectangle.Height - 6);
+
+            button2.Location = new System.Drawing.Point(ClientRectangle.Left + ClientRectangle.Width - ClientRectangle.Height, ClientRectangle.Top);
+            button2.Size = new System.Drawing.Size(ClientRectangle.Height, ClientRectangle.Height);
+        }
+    }
+}
