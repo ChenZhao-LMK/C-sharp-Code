a336280190cfd01791bc9cc2ad6b2f07183006ff
ChenZhao <chen_zhao_heu@163.com>
2019-11-13 17:03:48 +0800

    窗口拖拽

---
+++ 窗口区域拖动分配变化/多个panel拖动变化/多个panel拖动变化/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace 多个panel拖动变化
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(308, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(340, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(402, 100);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Cursor = System.Windows.Forms.Cursors.SizeWE;
+            this.label1.Location = new System.Drawing.Point(326, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(8, 100);
+            this.label1.TabIndex = 2;
+            this.label1.Text = " ";
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.GoDragging);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.OnDragging);
+            this.label1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OutDragging);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Cursor = System.Windows.Forms.Cursors.SizeNS;
+            this.label2.Location = new System.Drawing.Point(12, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(730, 8);
+            this.label2.TabIndex = 3;
+            this.label2.Text = " ";
+            this.label2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.GoDragging);
+            this.label2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OutDragging);
+            this.label2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.OnDragging);
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Location = new System.Drawing.Point(12, 126);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(392, 160);
+            this.panel3.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Cursor = System.Windows.Forms.Cursors.SizeWE;
+            this.label3.Location = new System.Drawing.Point(410, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(8, 160);
+            this.label3.TabIndex = 5;
+            this.label3.Text = " ";
+            this.label3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.GoDragging);
+            this.label3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OutDragging);
+            this.label3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.OnDragging);
+
+            // 
+            // panel4
+            // 
+            this.panel4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Location = new System.Drawing.Point(424, 126);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(318, 160);
+            this.panel4.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 298);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "panel变化";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
+
---
+++ 窗口区域拖动分配变化/多个panel拖动变化/多个panel拖动变化/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 多个panel拖动变化
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool Isdragging = false;
+
+        private void GoDragging(object sender, MouseEventArgs e)
+        {
+            Isdragging = true;
+        }
+        private void OutDragging(object sender, MouseEventArgs e)
+        {
+            Isdragging = false;
+        }
+
+        
+
+        private void OnDragging(object sender, MouseEventArgs e)
+        {
+            var selectedLabel = (Label)sender;
+            if (Isdragging)
+            {
+                double locationRatio;
+                switch (selectedLabel.Name)
+                {
+                    case "label1":
+                        locationRatio = (double)(label1.Left + e.X) / (panel1.Width + panel2.Width);
+                        if (locationRatio > 0.2 && locationRatio < 0.8)
+                        {
+                            label1.Left += e.X;
+
+                            panel1.Width += e.X;
+
+                            panel2.Left += e.X;
+                            panel2.Width -= e.X;
+
+                        }
+                        break;
+                    case "label2":
+                        locationRatio = (double)(label2.Top + e.Y) / (panel1.Height + panel3.Height);
+                        if (locationRatio > 0.2 && locationRatio < 0.8)
+                        {
+                            panel1.Height += e.Y;
+                            panel2.Height += e.Y;
+
+                            panel3.Top += e.Y;
+                            panel3.Height -= e.Y;
+                            panel4.Top += e.Y;
+                            panel4.Height -= e.Y;
+
+                            label2.Top += e.Y;
+                            label1.Height += e.Y;
+                            label3.Top += e.Y;
+                            label3.Height -= e.Y;
+                        }
+                        break;
+                    case "label3":
+                        locationRatio = (double)(label3.Left + e.X) / (panel3.Width + panel4.Width);
+                        if (locationRatio > 0.2 && locationRatio < 0.8)
+                        {
+                            label3.Left += e.X;
+
+                            panel3.Width += e.X;
+                            panel4.Left += e.X;
+                            panel4.Width -= e.X;
+                        }
+                        break;
+                }
+            }
+        }
+        
+    }
+
+}
